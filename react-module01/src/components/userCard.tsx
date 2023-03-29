7080c20ce18d778a78530ee24a45785906a45aab
Bonus156 <Bonus.by@mail.ru>
2023-03-29 13:59:32 +0300

    fix: formPageTest

--- react-module01/src/components/userCard.tsx
+++ react-module01/src/components/userCard.tsx
@@ -1,7 +1,7 @@
 import { UserInfo } from '../models/types';
 
 export function UserCard({ userName, birthday, country, isMale, image }: UserInfo) {
-  const appeal = isMale ? 'Mr ' : 'Mrs ';
+  const appeal = isMale ? 'Mr ' : 'Ms ';
   return (
     <div className="border py-2 px-2 rounded w-[calc((100%-1rem)/3)] flex flex-col items-center lg:w-[calc((100%-1.5rem)/4)]">
       <img src={URL.createObjectURL(image)} className="w-full" alt="avatar" />
--- react-module01/src/test/formPage.test.tsx
+++ react-module01/src/test/formPage.test.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { FormPage } from '../pages/formPage';
+import { CardsState } from '../models/types';
 
 describe('HomePage', () => {
-  test('is shows type text', () => {
-    render(<FormPage />);
+  test('is shows form type text with empty cardList', () => {
+    const cardList: CardsState = { cardsList: [] };
+    render(<FormPage setCards={cardList} />);
     expect(screen.getByText(/Your name/i)).toBeInTheDocument();
+    expect(screen.getByText(/Your birthday/i)).toBeInTheDocument();
+    expect(screen.getAllByRole('textbox')).toHaveLength(1);
+    expect(screen.getAllByRole('option')).toHaveLength(6);
   });
 });
