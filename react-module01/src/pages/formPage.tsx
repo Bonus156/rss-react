713a7c8a0d0e08ee66a376ef2295b8bdf4296b93
Bonus156 <Bonus.by@mail.ru>
2023-03-31 10:50:15 +0300

    feat: migrate FormPage from class to function

--- react-module01/src/pages/formPage.tsx
+++ react-module01/src/pages/formPage.tsx
@@ -1,6 +1,6 @@
-import React, { Component, createRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { validateForm, validateImageFile } from '../actions/validation';
-import { CardsState, UserInfo } from '../models/types';
+import { UserInfo } from '../models/types';
 import { FormCards } from '../components/formCards';
 
 type FormFields = {
@@ -12,93 +12,29 @@ type FormFields = {
   file: HTMLInputElement;
 };
 
-type Errors = {
-  errorName: string;
-  errorBirthday: string;
-  errorCountry: string;
-  errorAnswer: string;
-  errorUpload: string;
-  errorAgreement: string;
-};
+export function FormPage() {
+  const formRef = useRef<HTMLFormElement>(null);
+  const inputName = useRef<HTMLInputElement>(null);
+  const inputDate = useRef<HTMLInputElement>(null);
+  const selectCountry = useRef<HTMLSelectElement>(null);
+  const inputIsAgree = useRef<HTMLInputElement>(null);
+  const inputRadioMale = useRef<HTMLInputElement>(null);
+  const inputRadioFemale = useRef<HTMLInputElement>(null);
+  const inputFile = useRef<HTMLInputElement>(null);
 
-interface State {
-  selectValue: string | undefined;
-  errors: Errors;
-  cardInfo: UserInfo[];
-  confirmation: string;
-}
+  const [selectValue, setSelectVal] = useState('unselect');
+  const [errors, setErrorsText] = useState({
+    errorName: '',
+    errorBirthday: '',
+    errorCountry: '',
+    errorAnswer: '',
+    errorUpload: '',
+    errorAgreement: '',
+  });
+  const [cardInfo, setCards] = useState<UserInfo[]>([]);
+  const [confirmation, setConfirmText] = useState('');
 
-interface FormPageProps {
-  cards: CardsState;
-}
-
-export class FormPage extends Component<FormPageProps, State> {
-  inputName: React.RefObject<HTMLInputElement>;
-  inputDate: React.RefObject<HTMLInputElement>;
-  selectCountry: React.RefObject<HTMLSelectElement>;
-  inputIsAgree: React.RefObject<HTMLInputElement>;
-  inputRadioMale: React.RefObject<HTMLInputElement>;
-  inputRadioFemale: React.RefObject<HTMLInputElement>;
-  inputFile: React.RefObject<HTMLInputElement>;
-  formRef: React.RefObject<HTMLFormElement>;
-
-  constructor(props: FormPageProps) {
-    super(props);
-    this.state = {
-      selectValue: 'unselect',
-      errors: {
-        errorName: '',
-        errorBirthday: '',
-        errorCountry: '',
-        errorAnswer: '',
-        errorUpload: '',
-        errorAgreement: '',
-      },
-      cardInfo: [],
-      confirmation: '',
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.formRef = createRef<HTMLFormElement>();
-    this.inputName = createRef<HTMLInputElement>();
-    this.inputDate = createRef<HTMLInputElement>();
-    this.selectCountry = createRef<HTMLSelectElement>();
-    this.inputIsAgree = createRef<HTMLInputElement>();
-    this.inputRadioMale = createRef<HTMLInputElement>();
-    this.inputRadioFemale = createRef<HTMLInputElement>();
-    this.inputFile = createRef<HTMLInputElement>();
-  }
-
-  pushInfo(newCard: UserInfo) {
-    if (this.validateInput()) {
-      this.state.cardInfo.push(newCard);
-      this.setState({
-        confirmation: 'Submit Successfull',
-      });
-      setTimeout(() => {
-        this.setState({ confirmation: '' });
-      }, 3000);
-      this.formRef.current?.reset();
-    }
-  }
-
-  handleSubmit: React.FormEventHandler<HTMLFormElement & FormFields> = (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-    this.pushInfo({
-      userName: this.inputName.current?.value as string,
-      birthday: this.inputDate.current?.value as string,
-      country: this.selectCountry.current?.value as string,
-      isMale: !!this.inputRadioMale.current?.checked,
-      image: (this.inputFile.current?.files as FileList)[0],
-    });
-    this.setState({
-      selectValue: this.selectCountry.current?.value,
-    });
-  };
-
-  validateInput() {
+  const validateInput = useCallback(() => {
     const {
       isNameCorrect,
       isDateCorrect,
@@ -107,22 +43,20 @@ export class FormPage extends Component<FormPageProps, State> {
       isFileUploaded,
       isAgree,
     } = validateForm(
-      this.inputName.current?.value,
-      this.inputDate.current?.value,
-      this.selectCountry.current?.value as string,
-      !!this.inputRadioMale.current?.checked || !!this.inputRadioFemale.current?.checked,
-      validateImageFile((this.inputFile.current?.files as FileList)[0]),
-      !!this.inputIsAgree.current?.checked
+      inputName.current?.value,
+      inputDate.current?.value,
+      selectCountry.current?.value as string,
+      !!inputRadioMale.current?.checked || !!inputRadioFemale.current?.checked,
+      validateImageFile((inputFile.current?.files as FileList)[0]),
+      !!inputIsAgree.current?.checked
     );
-    this.setState({
-      errors: {
-        errorName: isNameCorrect ? '' : 'incorrect name',
-        errorBirthday: isDateCorrect ? '' : 'incorrect date',
-        errorCountry: isCountrySelected ? '' : 'you must select a country',
-        errorAnswer: isGenderSelected ? '' : 'gender must be selected',
-        errorUpload: isFileUploaded ? '' : 'upload .jpg or .pdf file',
-        errorAgreement: isAgree ? '' : 'your agreement is required',
-      },
+    setErrorsText({
+      errorName: isNameCorrect ? '' : 'incorrect name',
+      errorBirthday: isDateCorrect ? '' : 'incorrect date',
+      errorCountry: isCountrySelected ? '' : 'you must select a country',
+      errorAnswer: isGenderSelected ? '' : 'gender must be selected',
+      errorUpload: isFileUploaded ? '' : 'upload .jpg or .pdf file',
+      errorAgreement: isAgree ? '' : 'your agreement is required',
     });
     return (
       isNameCorrect &&
@@ -132,93 +66,115 @@ export class FormPage extends Component<FormPageProps, State> {
       isFileUploaded &&
       isAgree
     );
-  }
+  }, []);
 
-  render() {
-    const { errorName, errorBirthday, errorCountry, errorAnswer, errorUpload, errorAgreement } =
-      this.state.errors;
-    const confirmation = this.state.confirmation;
-    return (
-      <>
-        <form
-          className="container mx-auto flex flex-col"
-          ref={this.formRef}
-          onSubmit={this.handleSubmit}
-        >
-          <label>
-            <span>Your name</span>
-            <input
-              className="cursor-text border rounded px-4 py-2 m-2"
-              name="name"
-              type="text"
-              ref={this.inputName}
-            />
-            <span className="text-red-700">{errorName}</span>
+  const pushInfo = useCallback(
+    (newCard: UserInfo) => {
+      if (validateInput()) {
+        setCards([...cardInfo, newCard]);
+        setConfirmText('Submit Successfull');
+        setTimeout(() => {
+          setConfirmText('');
+        }, 3000);
+        formRef.current?.reset();
+      }
+    },
+    [validateInput, cardInfo]
+  );
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement & FormFields> = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setSelectVal(selectCountry.current?.value as string);
+      pushInfo({
+        userName: inputName.current?.value as string,
+        birthday: inputDate.current?.value as string,
+        country: selectValue,
+        isMale: !!inputRadioMale.current?.checked,
+        image: (inputFile.current?.files as FileList)[0],
+      });
+    },
+    [pushInfo, selectValue]
+  );
+
+  const { errorName, errorBirthday, errorCountry, errorAnswer, errorUpload, errorAgreement } =
+    errors;
+  return (
+    <>
+      <form className="container mx-auto flex flex-col" ref={formRef} onSubmit={handleSubmit}>
+        <label>
+          <span>Your name</span>
+          <input
+            className="cursor-text border rounded px-4 py-2 m-2"
+            name="name"
+            type="text"
+            ref={inputName}
+          />
+          <span className="text-red-700">{errorName}</span>
+        </label>
+        <label>
+          <span>Your birthday</span>
+          <input
+            className="cursor-text border rounded px-4 py-2 m-2"
+            name="date"
+            type="date"
+            ref={inputDate}
+          />
+          <span className="text-red-700">{errorBirthday}</span>
+        </label>
+        <label>
+          <span>Choose country:</span>
+          <select
+            className="cursor-pointer border rounded px-4 py-2 m-2"
+            name="country"
+            ref={selectCountry}
+          >
+            <option value="unselect">Choose here</option>
+            <option value="belarus">Belarus</option>
+            <option value="ukraine">Ukraine</option>
+            <option value="poland">Poland</option>
+            <option value="russia">Russia</option>
+            <option value="other">Other</option>
+          </select>
+          <span className="text-red-700">{errorCountry}</span>
+        </label>
+        <div>
+          <label className="cursor-pointer mr-4 my-4">
+            <input className="mr-2" type="radio" name="question" ref={inputRadioMale} />
+            Male
           </label>
-          <label>
-            <span>Your birthday</span>
-            <input
-              className="cursor-text border rounded px-4 py-2 m-2"
-              name="date"
-              type="date"
-              ref={this.inputDate}
-            />
-            <span className="text-red-700">{errorBirthday}</span>
+          <label className="cursor-pointer mr-4 my-4">
+            <input className="mr-2" type="radio" name="question" ref={inputRadioFemale} />
+            Female
           </label>
-          <label>
-            <span>Choose country:</span>
-            <select
-              className="cursor-pointer border rounded px-4 py-2 m-2"
-              name="country"
-              ref={this.selectCountry}
-            >
-              <option value="unselect">Choose here</option>
-              <option value="belarus">Belarus</option>
-              <option value="ukraine">Ukraine</option>
-              <option value="poland">Poland</option>
-              <option value="russia">Russia</option>
-              <option value="other">Other</option>
-            </select>
-            <span className="text-red-700">{errorCountry}</span>
-          </label>
-          <div>
-            <label className="cursor-pointer mr-4 my-4">
-              <input className="mr-2" type="radio" name="question" ref={this.inputRadioMale} />
-              Male
-            </label>
-            <label className="cursor-pointer mr-4 my-4">
-              <input className="mr-2" type="radio" name="question" ref={this.inputRadioFemale} />
-              Female
-            </label>
-            <span className="text-red-700">{errorAnswer}</span>
-          </div>
-          <label className="mr-2">
-            Upload image:
-            <input
-              className="cursor-text border rounded px-4 py-2 m-2"
-              name="file"
-              accept="image/*"
-              type="file"
-              ref={this.inputFile}
-            />
-            <span className="text-red-700">{errorUpload}</span>
-          </label>
-          <label className="cursor-pointer">
-            <span>Agree:</span>
-            <input className="m-2" name="isAgree" type="checkbox" ref={this.inputIsAgree} />
-            <span className="text-red-700">{errorAgreement}</span>
-          </label>
-          <div>
-            <input
-              className="cursor-pointer border font-semibold rounded px-4 py-2 w-fit bg-green-400 hover:bg-green-500"
-              type="submit"
-              value="Submit"
-            />
-            <span className="ml-2 font-bold text-green-800">{confirmation}</span>
-          </div>
-        </form>
-        <FormCards cardsList={this.state.cardInfo} />
-      </>
-    );
-  }
+          <span className="text-red-700">{errorAnswer}</span>
+        </div>
+        <label className="mr-2">
+          Upload image:
+          <input
+            className="cursor-text border rounded px-4 py-2 m-2"
+            name="file"
+            accept="image/*"
+            type="file"
+            ref={inputFile}
+          />
+          <span className="text-red-700">{errorUpload}</span>
+        </label>
+        <label className="cursor-pointer">
+          <span>Agree:</span>
+          <input className="m-2" name="isAgree" type="checkbox" ref={inputIsAgree} />
+          <span className="text-red-700">{errorAgreement}</span>
+        </label>
+        <div>
+          <input
+            className="cursor-pointer border font-semibold rounded px-4 py-2 w-fit bg-green-400 hover:bg-green-500"
+            type="submit"
+            value="Submit"
+          />
+          <span className="ml-2 font-bold text-green-800">{confirmation}</span>
+        </div>
+      </form>
+      <FormCards cardsList={cardInfo} />
+    </>
+  );
 }
