a74cd77d75dfd8c8067002255e73b72db48ba3da
Bonus156 <Bonus.by@mail.ru>
2023-04-08 18:39:28 +0300

    feat: add overflow-hidden for body onModalOpen

--- react-module01/src/components/modal.tsx
+++ react-module01/src/components/modal.tsx
@@ -15,47 +15,52 @@ export function Modal({ hero, isVisible, setVisible }: ModalProps) {
   return (
     <div
       className={
-        isVisible ? 'flex flex-col block w-full h-full backdrop-blur-lg fixed inset-0' : 'hidden'
+        isVisible
+          ? 'modal flex flex-col block justify-center w-full h-full backdrop-blur-lg fixed inset-0'
+          : 'hidden'
       }
       onClick={handleClick}
     >
-      <div className="inline-block self-end m-4 cursor-pointer hover:scale-105">
-        <svg
-          width="50px"
-          height="50px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-            stroke="#292D32"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M9.16998 14.83L14.83 9.17004"
-            stroke="#292D32"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M14.83 14.83L9.16998 9.17004"
-            stroke="#292D32"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </div>
       <div
-        className="border bg-white py-2 px-2 rounded w-1/2 flex flex-col items-center m-auto lg:w-1/3"
+        className="border bg-white py-2 px-2 rounded w-1/2 flex flex-col items-center mx-auto lg:w-1/3"
         onClick={(e: MouseEvent<HTMLDivElement>) => e.stopPropagation()}
       >
+        <div
+          className="inline-block self-end absolute cursor-pointer hover:scale-105"
+          onClick={handleClick}
+        >
+          <svg
+            width="40px"
+            height="40px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+              stroke="#d2d2d2"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M9.16998 14.83L14.83 9.17004"
+              stroke="#292D32"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M14.83 14.83L9.16998 9.17004"
+              stroke="#292D32"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
         <img src={hero.image} className="w-full" alt={hero.name} />
-        <p className="font-bold ">
+        <p className="font-bold text-lg">
           <span className="font-normal">Name: </span>
           {hero.name}
         </p>
@@ -64,6 +69,8 @@ export function Modal({ hero, isVisible, setVisible }: ModalProps) {
         <p>Species: {hero.species}</p>
         <p>Gender: {hero.gender}</p>
         <p>Location: {hero.location.name}</p>
+        <p className="text-sm">Created: {new Date(hero.created).toDateString()}</p>
+        <p className="my-2 text-sm self-start">ID: {hero.id}</p>
       </div>
     </div>
   );
