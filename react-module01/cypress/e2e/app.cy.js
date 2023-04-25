a9dc734d58d2fc767478970d1020516fd7a17a71
Bonus156 <Bonus.by@mail.ru>
2023-04-25 18:18:07 +0300

    feat: add more e2e tests

--- react-module01/cypress/e2e/app.cy.js
+++ react-module01/cypress/e2e/app.cy.js
@@ -11,6 +11,35 @@ describe('Home Page', () => {
     cy.get('input').type(findName);
     cy.get('input').should('have.value', findName);
     cy.get('button').click();
+    cy.get('div.container div:only-of-type').children().should('contain.text', findName);
+  });
+  it('should show the same Monsters after visiting form page and returning back', () => {
+    const findName = 'Monster';
+    cy.get('input').type(findName);
+    cy.get('input').should('have.value', findName);
+    cy.get('button').click();
     cy.get('.container div:only-of-type').children().should('contain.text', findName);
+    cy.get('a:last-of-type').click();
+    cy.get('a:first-of-type').click();
+    cy.get('input').should('have.value', findName);
+    cy.get('div.container div:only-of-type').children().contains(findName);
+  });
+  it('should show modal after click on card', () => {
+    cy.get('div.container div:only-of-type')
+      .children()
+      .first()
+      .should('contain.text', 'Name')
+      .click();
+    cy.get('.modal').should('exist');
+  });
+  it('should close modal after click on overlay but not on center', () => {
+    cy.get('div.container div:only-of-type')
+      .children()
+      .first()
+      .should('contain.text', 'Name')
+      .click();
+    cy.get('.modal').click('center');
+    cy.get('.modal').should('exist').click('topLeft');
+    cy.get('.modal').should('not.exist');
   });
 });
